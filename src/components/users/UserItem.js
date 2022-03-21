20847b78d329bbfd05abeebe78ba656c7703595a
James Suescun <james.steven2318@gmail.com>
2022-03-21 18:01:49 -0400

    use desctructing assignment to declare state varaibles in render()

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 
 export class UserItem extends Component {
-  constructor () {
-      super();
-      this.state = {
+  state = {
           id: 'id',
           login: 'mojombo',
           avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
           html_url: 'https://github.com/mojombo'
       }
-  }
-  
+
     render() {
+        const {login, avatar_url, html_url} = this.state;
     return (
-      <div>userItem</div>
-    )
+      <div className='card text-center'>
+          <img src={avatar_url} 
+           alt='' 
+           className='round-img' 
+           style={{width: '60px'}} />
+           <h3>{login}</h3>
+
+           <div>
+               <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                   more
+               </a>
+           </div>
+      </div>
+      
+      )
   }
 }
 
