ff57d62d49c4a0ff0e928d12daf88619e30997bb
James Suescun <james.steven2318@gmail.com>
2022-03-22 17:48:07 -0400

    include element that will allow app to not run out of requests

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id=
+    ${ process.ev.REACT_APP_GITHUB_CLIENT_ID }&client_secret=
+    ${ process.ev.REACT_APP_GITHUB_CLIENT_SECRET }`);
+    
     this.setState({ users: res.data, loading: false });
   }
 
