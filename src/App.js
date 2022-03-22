2fe85a9d24d566d79b6e1a5c682d7b827429400e
James Suescun <james.steven2318@gmail.com>
2022-03-21 23:55:43 -0400

    add styles to Users component using a variable

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
     return(
       <div className="App">
           <NavBar title='github finder'/>
-          <Users />
+          <div className='container'>
+            <Users />
+          </div>
       </div>
     );
   }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -27,7 +27,7 @@ class Users extends Component {
 
     render() {
         return (
-            <div>
+            <div style={ userStyle }>
                 {this.state.users.map(user => (
                     <UserItem key={ user.id } user={ user }/>
                 ))}
@@ -36,5 +36,10 @@ class Users extends Component {
     }
 }
 
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem' 
+}
 export default Users;
 
