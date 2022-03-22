321e200a4f0b80f3c1112bf8adee117965f50ba8
James Suescun <james.steven2318@gmail.com>
2022-03-22 10:35:01 -0400

    install promise based HTTP client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import NavBar from './components/layout/NavBar.js';
 import Users from './components/users/Users.js';
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users')
+      .then(res => console.log(res.data));
+  }
+
   render () {
     return(
       <div className="App">
