6d9a4726899f6cb86d4a256745c97b826630d9b7
James Suescun <james.steven2318@gmail.com>
2022-03-23 14:35:32 -0400

    App fetches user data from GitHub

--- src/App.js
+++ src/App.js
@@ -12,14 +12,13 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true });
-
-    const res = await axios.get(`https://api.github.com/users?client_id=
-    ${ process.ev.REACT_APP_GITHUB_CLIENT_ID }&client_secret=
-    ${ process.ev.REACT_APP_GITHUB_CLIENT_SECRET }`);
     
-    this.setState({ users: res.data, loading: false });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${ text }&client_id=${ process.env.REACT_APP_GITHUB_CLIENT_ID }&client_secret=${ process.env.REACT_APP_GITHUB_CLIENT_SECRET }`);
+    
+    this.setState({ users: res.data.items, loading: false });
   }
 
   render () {
@@ -27,7 +26,7 @@ class App extends Component {
       <div className="App">
           <NavBar title='github finder'/>
           <div className='container'>
-            <Search />
+            <Search searchUsers={ this.searchUsers }/>
             <Users loading={ this.state.loading } users={ this.state.users }/>
           </div>
       </div>
@@ -36,3 +35,16 @@ class App extends Component {
 }
 
 export default App;
+
+// async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${ process.env.REACT_APP_GITHUB_CLIENT_ID }&client_secret=${ process.env.REACT_APP_GITHUB_CLIENT_SECRET }`);
+    
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // example of componentDidMount that was used to
+  // populate hard coded data to serve as a placeholder 
+  
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
       text: '',
   }
   
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  }
+  
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   onSubmit = (e) => {
       e.preventDefault();
-      
+      this.props.searchUsers( this.state.text );
+      this.setState({ text: '' });
   }
   
     render() {
     return (
-      <form className='form'>
+      <form className='form' onSubmit={ this.onSubmit } >
           <input type='text' 
                  name='text' 
                  placeholder='search users..' 
                  value={ this.state.text }
                  onChange={ this.onChange }
-                 onSubmit={ this.onSubmit }/>
+                 />
         
         <input type='submit'
                value='search'
@@ -30,3 +36,17 @@ export class Search extends Component {
 }
 
 export default Search
+
+// onSubmit prop in input passes onSubmit funciton
+// onSubmit funciton will prevent form from being submitted
+// pass a func as props with the text using state
+// then use setter function function to clear input (state)
+// 
+// In this case the passed up 'function' to (App)
+// however, function actions are not defined
+// this function simply passes the state then 
+// clears the state  
+//
+// actions must defined within the component in which
+// the button will be used in
+// In this case the App component
