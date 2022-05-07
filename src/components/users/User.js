ba8817d8cc96bcec958902216c183f9e15ec5167
James Suescun <james.steven2318@gmail.com>
2022-05-07 17:43:41 -0400

    complete user card with data from fetcch api

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -27,6 +27,7 @@ export class User extends Component {
             following,
             public_repos,
             public_gists,
+            company,
             hireable
         } = this.props.user;
 
@@ -40,7 +41,93 @@ export class User extends Component {
                     Back to search
                 </Link>
                 Hireable: { " " }
-                {!hireable ? <i className="fas fa-check text-sucess" /> : <i className="fas fa-times-circle text-danger" /> }
+                {hireable ? <i className="fas fa-check text-sucess" /> : <i className="fas fa-times-circle text-danger" /> }
+
+                <div className="card grid-2">
+                    <div classname="all-center">
+                        <img src={avatar_url} className="round-img" alt=" " style={{ width: "150px"}} />
+                        <h1>{ name }</h1>
+                        <p>Location: { !location ? <span> unknown </span>  : location }</p>
+                    </div>
+                    <div>
+                        {bio ? (
+                            <>
+                                <h3>BIO</h3>
+                                { bio }
+                            </>
+                        ) : (<>
+                                <h3>BIO</h3>
+                                <p>No bio available</p>
+                            </>)
+                    }
+                    <a href={ html_url} className="btn btn-dark my-1">
+                        Visit Github Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login ? (
+                                <>
+                                    <strong> Username:</strong> { login }
+                                </>
+                            ) : (
+                                <>
+                                <strong> Username:</strong> 
+                                <span>
+                                      {"  no username found"}
+                                </span>
+                                </>
+                                )
+                            }
+                        </li>
+                        <li>
+                            {blog ? (
+                                <>
+                                    <strong> Website:</strong> { blog }
+                                </>
+                            ) : (
+                                <>
+                                <strong> Website:</strong> 
+                                <span>
+                                    {"  no website found"}
+                                </span>
+                                </>
+                            )
+                        }
+                        </li>
+
+                        <li>
+                            {company ? (
+                                <>
+                                    <strong> Company:</strong> { company }
+                                </>
+                            ) : (
+                                <>
+                                <strong> Company:</strong> 
+                                <span>
+                                    {"  no company found"}
+                                </span>
+                                </>
+                            )
+                        }
+                        </li>
+                    </ul>
+
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className="badge badge-primary">
+                        Followers: { followers }
+                    </div>
+                    <div className="badge badge-success">
+                        Following: { following }
+                    </div>
+                    <div className="badge badge-light">
+                        Public Repos: { public_repos }
+                    </div>
+                    <div className="badge badge-dark">
+                        Followers: { public_gists }
+                    </div>
+                </div>
             </>
         )
     }
