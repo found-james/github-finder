21c50c781f09b23f28da5d96e2edf69718a8040d
James Suescun <james.steven2318@gmail.com>
2022-05-09 13:47:50 -0400

    create Home and NotFound pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "./components/layout/NavBar.js";
-import  Alert from "./components/layout/Alert.js";
-
-import Users from "./components/users/Users.js";
-import User from "./components/users/User.js";
-
-import Search from "./components/users/Search.js";
+import Alert from "./components/layout/Alert.js";
 import About from "./components/pages/About.js";
-
+import Home from "./components/pages/Home.js";
+import NotFound from "./components/pages/NotFound.js";
+import User from "./components/users/User.js";
 import GithubState from "./context/github/GithubState.js";
 import AlertState from "./context/alert/AlertState.js";
 import "./App.css";
@@ -26,17 +23,10 @@ export default function App () {
                 <div className="container">
                   <Alert />
                   <Switch>
-
-                    <Route exact path="/" render={ props => (
-                      <>
-                        <Search />
-                        <Users />
-                      </>
-                    )} />
-
+                    <Route exact path="/" component={Home} />
                     <Route exact path="/about" component={About} />
-                    <Route  path="/user/:login" component={User} />
-
+                    <Route exact path="/user/:login" component={User} />
+                    <Route component={NotFound} />
                   </Switch>
                 </div>
             </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+
+export default function NotFound () {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p className="lead">This page does not seem to exist</p>
+        </div>
+    );
+};
+
